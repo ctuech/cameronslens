d6ec5c7a64ae5b519b8dac1c90d8549dbf513894
Matthew Harris <mharr171.z@gmail.com>
2018-03-30 17:27:31 -0700

    add post index and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  get 'post/index'
+
+  get 'post/show'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root 'welcome#index'
 
-  get 'posts/index'
-
-  get 'posts/show'
+  resources :posts
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
